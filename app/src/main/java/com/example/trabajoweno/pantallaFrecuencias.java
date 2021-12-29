85f7ebc647c1f9c24842c8917a07c8c4c6f0d0f4
javixxxx <javier.ruizr@alumnos.upm.es>
2021-12-29 01:55:23 +0100

    trabajoweno

--- app/src/main/java/com/example/trabajoweno/pantallaFrecuencias.java
+++ app/src/main/java/com/example/trabajoweno/pantallaFrecuencias.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.view.WindowManager;
 import android.widget.Button;
 
 public class pantallaFrecuencias extends AppCompatActivity {
@@ -12,6 +13,7 @@ public class pantallaFrecuencias extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_pantalla_frecuencias);
         Button altas = (Button) findViewById(R.id.boton2);
 
