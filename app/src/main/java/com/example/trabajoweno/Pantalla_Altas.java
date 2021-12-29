9b20f0b22b87a37e77403ba387575a80c9ae636c
javixxxx <javier.ruizr@alumnos.upm.es>
2021-12-29 01:38:30 +0100

    trabajoweno

--- app/src/main/java/com/example/trabajoweno/Pantalla_Altas.java
+++ app/src/main/java/com/example/trabajoweno/Pantalla_Altas.java
@@ -10,5 +10,6 @@ public class Pantalla_Altas extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pantalla_altas);
+
     }
 }
