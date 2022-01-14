37357c60fe89b7bf6c8c22bafb11745d0acc9e5e
javixxxx <javier.ruizr@alumnos.upm.es>
2022-01-14 19:19:13 +0100

    trabajoweno

--- app/src/main/java/com/example/trabajoweno/Pantalla_Altas.java
+++ app/src/main/java/com/example/trabajoweno/Pantalla_Altas.java
@@ -1,17 +1,44 @@
 package com.example.trabajoweno;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.view.View;
 import android.view.WindowManager;
+import android.widget.Button;
 
 public class Pantalla_Altas extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pantalla_altas); /** No se como se llamara tu layout, he puesto main_activity, pero puede ser otra */
+        Button buttonstart = (Button) findViewById(R.id.buttonstart);
+        Button buttonstop = (Button) findViewById(R.id.buttonstop);
+        /*AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(this);*/
+        MediaPlayer mMediaPlayer = MediaPlayer.create(Pantalla_Altas.this, R.raw.sonido_pitido); /** El nombre lo he puesto aleatorio, tienes que poner el nombre del tuyo, MyActivity.this,R.raw.aleatorio */
+        /** tienes que meter tu sonido en "app/src/main/res/raw/" */
+        buttonstart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
 
-        setContentView(R.layout.activity_pantalla_altas);
+                if (v.getId() == R.id.buttonstart) {
+                    mMediaPlayer.start();
+                }
+            }
+        });
 
+        buttonstop.setOnClickListener(new View.OnClickListener(){
+
+            public void onClick(View v) {
+                if (v.getId() == R.id.buttonstop) {
+                    mMediaPlayer.stop();
+                    /*displaySeconds();*/
+                }
+            }
+        });
     }
 }
--- app/src/main/java/com/example/trabajoweno/app.java
+++ app/src/main/java/com/example/trabajoweno/app.java
@@ -1,4 +1,4 @@
-package com.example.trabajoweno;
+/*package com.example.trabajoweno;
 
 import android.content.Context;
 import android.media.AudioManager;
@@ -11,27 +11,28 @@ import android.widget.TextView;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
-public class app extends AppCompatActivity { /** MyActivity o como lo quieras llamar */
+public class app extends AppCompatActivity {
     
     public void setMaxVolume(Context mContext) {
         AudioManager am = (AudioManager) mContext.getSystemService(Context.AUDIO_SERVICE);
         am.setStreamVolume(AudioManager.STREAM_MUSIC, am.getStreamMaxVolume(AudioManager.STREAM_MUSIC), 0);
     }
 
-    Button buttonstart = (Button) this.findViewById(R.id.buttonstart);
-    Button buttonstop = (Button) this.findViewById(R.id.buttonstop);
+    Button buttonstart = (Button) findViewById(R.id.buttonstart);
+    Button buttonstop = (Button) findViewById(R.id.buttonstop);
     AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(this);
 
-    MediaPlayer mMediaPlayer;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pantalla_altas); /** No se como se llamara tu layout, he puesto main_activity, pero puede ser otra */
-        mMediaPlayer = MediaPlayer.create(app.this,R.raw.sonido_pitido); /** El nombre lo he puesto aleatorio, tienes que poner el nombre del tuyo, MyActivity.this,R.raw.aleatorio */
+        /*MediaPlayer mMediaPlayer = MediaPlayer.create(app.this,R.raw.sonido_pitido); /** El nombre lo he puesto aleatorio, tienes que poner el nombre del tuyo, MyActivity.this,R.raw.aleatorio */
         /** tienes que meter tu sonido en "app/src/main/res/raw/" */
-        buttonstart.setOnClickListener(new View.OnClickListener(){
-
+        /*buttonstart.setOnClickListener(new View.OnClickListener(){
+            @Override
             public void onClick(View v) {
+
                 if (v.getId() == R.id.buttonstart) {
                     mMediaPlayer.start();
                 } 
@@ -57,10 +58,10 @@ public class app extends AppCompatActivity { /** MyActivity o como lo quieras ll
             @Override
             public void onCompletion(MediaPlayer mediaPlayer) {
                 TextView secondsText = (TextView) findViewById(R.id.textView);
-                /*secondsText.setText(String.valueOf(Seconds));*/
+                secondsText.setText(String.valueOf(Seconds));
                 if(timeElapsed>4) secondsText.setText("@string/apto");
                 else secondsText.setText("@string/no_apto");
             }
         });
     }
-}  
+}*/
